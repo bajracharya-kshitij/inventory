ecfcf4de1cf9dd210f5eaf17d6f80b51f1921435
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2020-01-07 21:52:53 +0545

    Add react-bootstrap

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
 import ProductImage from './ProductImage';
 import ProductDescription from './ProductDescription';
 
 const Product = () => {
 	return (
-		<div>
-			<ProductImage />
-			<ProductDescription />
-		</div>
+		<Container>
+			<Row>
+				<Col sm={6}>
+					<ProductImage />
+				</Col>
+				<Col sm={6}>
+					<ProductDescription />
+				</Col>
+			</Row>
+		</Container>
 	);
 };
 export default Product;
