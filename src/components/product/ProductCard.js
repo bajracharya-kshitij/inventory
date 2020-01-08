27bda7d5d6b0925ea52cddb48031585302d8d9c2
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2020-01-08 08:33:11 +0545

    Add css to highlight card on hover

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -4,7 +4,7 @@ import ProductImage from './ProductImage';
 const ProductCard = (props) => {
 
 	return (
-		<div className="text-center pd-t-20 clickable" onClick={() => props.updateSelectedItem(props.item)}>
+		<div className="product-card" onClick={() => props.updateSelectedItem(props.item)}>
 			<ProductImage image={props.item.image} name={props.item.name} />
 			<h6>{props.item.name}</h6>
 		</div>
