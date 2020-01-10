a37cd45dfe807cc942e8d79572c4804355f73411
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2020-01-10 23:49:03 +0545

    Add validation for Product

--- back-end/src/main/java/np/com/sinew/inventory/controller/ProductController.java
+++ back-end/src/main/java/np/com/sinew/inventory/controller/ProductController.java
@@ -1,17 +1,11 @@
 package np.com.sinew.inventory.controller;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import np.com.sinew.inventory.model.Product;
 import np.com.sinew.inventory.service.ProductService;
@@ -23,9 +17,9 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 
-	@PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Product create(@RequestBody String data) throws JsonParseException, JsonMappingException, IOException {
-		Product product = new ObjectMapper().readValue(data, Product.class);
+	@PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, 
+					produces = MediaType.APPLICATION_JSON_VALUE)
+	public Product create(@RequestBody Product product) {
 		return productService.create(product);
 	}
 }
--- back-end/src/main/java/np/com/sinew/inventory/model/Product.java
+++ back-end/src/main/java/np/com/sinew/inventory/model/Product.java
@@ -7,16 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity(name = "product")
 @Data
 @NoArgsConstructor @AllArgsConstructor
-@Builder
 public class Product {
 
 	@Id
@@ -24,12 +26,19 @@ public class Product {
 	@Column(name="id", nullable=false, updatable=false)
 	private Long id;
 	
-	@Column(name="name")	
+	@NotBlank(message = "Name must not be blank")
+	@NotNull(message = "Name must not be empty")
+	@Size(min = 2, max = 32, message = "Name must be between 2 and 32 characters long")
+	@Column(name="name")
 	private String name;
 	
+	@NotBlank(message = "Company name must not be blank")
+	@NotNull(message = "Company name must not be empty")
+	@Size(min = 2, max = 32, message = "Company name must be between 2 and 32 characters long")
 	@Column(name="company")
 	private String company;
 	
+	@DecimalMin(value="0.0", inclusive=false, message="Price must be greater than 0")
 	@Column(name="price")
 	private BigDecimal price;
 }
